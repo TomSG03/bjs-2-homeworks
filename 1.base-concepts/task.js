3e8a4b511801419c3034c170dd6af67bf140b3e3
Tomilov Sergey <tomexpert@yandex.ru>
2021-05-20 16:09:30 +0800

    1. Первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  'use strict'
+
+  let arr = [];
+  let x;
+  let D = b**2 - 4 * a * c;
+  if (D === 0) {
+    x = -b / (2 * a);
+    arr.push(x);
+  }
+  else if (D > 0) {
+    x = (-b + Math.sqrt(D)) / (2 * a);
+    arr.push(x);
+    x = (-b - Math.sqrt(D)) / (2 * a);
+    arr.push(x);
+  }
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  'use strict'
+  
   let totalAmount;
 
   // код для задачи №2 писать здесь
