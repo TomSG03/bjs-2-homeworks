a23553cf3b098ad5f1e36bbd9ca3bca19195fdd1
Tomilov Sergey <tomexpert@yandex.ru>
2021-05-26 10:49:34 +0800

    Задания по теме - Функции

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,25 +2,44 @@
 function getArrayParams(arr) {
   let min,max,sum,avg;
 
-  // Ваш код
+  sum = arr[0];
+  min = arr[0];
+  max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (min > arr[i]) { 
+      min = arr[i]
+    }
+    if (max < arr[i]) { 
+      max = arr[i]
+    }
+    sum += arr[i];
+  };
+
+  avg = (sum / arr.length).toFixed(2);
 
   return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  arr.forEach(element => {
+    sum += element;  
+  });
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = func(arrOfArr[0]);
 
-  // Ваш кода
-  // for ...
-  
+  for (let i = 1; i < arrOfArr.length; i++) {
+    let n = func(arrOfArr[i]);
+    if (max < n) {
+      max = n
+    }
+  }
+
   return max
 }
