4307691603dc2a639545e041cebf16a460880ef8
Tomilov Sergey <tomexpert@yandex.ru>
2021-05-27 09:50:22 +0800

    3. Массивы - Оптимизация кода

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,18 +1,26 @@
-function compareArrays(arr1, arr2) {
+compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i]);
+
+advancedFilter = (arr) => arr.filter((item) => item >= 0).filter((item) => (item % 3) === 0).map((item) => item * 10);
+
+//  Это тоже работает. Оставлю на всякий случай.
+
+// function compareArrays(arr1, arr2) {
+//   return arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i]);
+// }
+
+// function advancedFilter(arr) {
+//   return arr.filter((item) => item >= 0).filter((item) => (item % 3) === 0).map((item) => item * 10);
+// }
+
+//function compareArrays(arr1, arr2) {
   //let result = false;
 
   //if (arr1.length === arr2.length) {
-  //  let compareArr = arr1.every((n, i) => n === arr2[i]);   //  Это тоже работаетю. Оставлю на всякий случай.
+  //  let compareArr = arr1.every((n, i) => n === arr2[i]);   
   //  if (compareArr === true) {
   //    result = true
   // }
   //}
 
   //return result; // boolean
-
-  return arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
-}
-
-function advancedFilter(arr) {
-  return arr.filter((item) => item >= 0).filter((item) => (item % 3) === 0).map((item) => item * 10);
-}
+//}
