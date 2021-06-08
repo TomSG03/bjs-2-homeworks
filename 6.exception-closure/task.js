61c5d9332fdbec45cdcc62a7a9a3bd654b0d6640
Tomilov Sergey <tomexpert@yandex.ru>
2021-06-09 06:37:49 +0800

    6. Обработка исключений и замыкания

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,64 @@
+//Задача №1. Форматтер чисел
+
+parseCount = (value) => { 
+  if (Number.isNaN(result = Number.parseInt(value))) {
+    throw new Error("Невалидное значение") 
+  }  
+  return result;
+};
+
+function validateCount(value) {
+  try {
+    result = parseCount(value);
+  } catch (error) {
+    result = error;
+  }
+  return result;
+}
+
+// console.log(parseCount('90'));
+// console.log(parseCount('zzz'));
+// console.log(validateCount('80'));
+// console.log(validateCount('www'));
+
+//Задача №2. Треугольник
+
+class Triangle {
+  constructor(a, b, c) {
+    if (a < b + c && b < a + c && c < a + b) {
+      this.a = a;
+      this.b = b;
+      this.c = c;
+    }
+    else {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+  }
+
+  getPerimeter() {
+    return this.a + this.b + this.c;
+  }  
+
+  getArea() {
+      let p = this.getPerimeter() / 2;
+      return Number( Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+   }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);    
+  } catch (error) {
+    return {
+      getPerimeter: () => 'Ошибка! Треугольник не существует',
+      getArea: () => 'Ошибка! Треугольник не существует'
+    }
+  }
+}
+
+// tr = new Triangle(1, 100, 3);
+// getTriangle(1, 100, 3);
+// console.log(`Периметр = ${tr.getPerimeter()}`);
+// console.log(`Периметр = ${tr.getArea()}`);
+
+
